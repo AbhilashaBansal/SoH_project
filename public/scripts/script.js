6e56e3d9218ed96a1ee89c5e015483202eba74cc
Abhilasha Bansal <64535297+AbhilashaBansal@users.noreply.github.com>
2021-03-28 03:01:58 +0530

    update final css

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,22 +1,20 @@
 let hospital_data;
 async function getData(){
     await $.get('/show_hospitals', (data)=>{
-        console.log("Hospitals Data: ", data);
+        // console.log("Hospitals Data: ", data);
         hospital_data = data;
     })
 }
 
 // const str1 = '<div class="col-lg-4 col-md-5 col-sm-6 col-xs-12"> <div class="card"> <img class="card-img-top" src="./Images/sample_photo.png" alt="Card image"> <div class="card-body"> <h5 class="card-title">';
-const str = '<li> </li>'
 
 getData().then(()=>{
     for(let i=0; i<hospital_data.length; i++){
         $('#hospital-list').append(
-            '<li style="margin:2%;">' + hospital_data[i].name + ' ' + hospital_data[i].city + ' ' + hospital_data[i].locality + ' <a href="/hosp?id=' + hospital_data[i].id + '" id="' + hospital_data[i].id + '" class="hosp btn btn-primary"' + '> View More </a> </li>'
-            // str1 + freedata[i].name + '</h5> <p class="card-text">Field: ' + freedata[i].domain + '</p>' +
-            // '<p class="card-text">Skills: ' + freedata[i].skills + '</p>' +
-            // '<p class="card-text">Projects: ' + freedata[i].projects + '</p>' +
-            // '<a href="/profiles/free?id=' + freedata[i].id + '" class="btn btn-primary">View Profile</a> </div> </div>' 
+            '<li style="padding:2%; margin: 10px 0; background-color:lightblue; border-radius:25px">' + hospital_data[i].name + ', ' + hospital_data[i].locality + ', ' + hospital_data[i].city + 
+            '</li> <a href="/hosp?id=' + hospital_data[i].id + '" id="' + hospital_data[i].id + '" class="hosp btn btn-primary"' + 
+            '> View More </a>'
+
         );
     }
 }).catch((err)=>{
